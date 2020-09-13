ecf7b0a02c98ccdd7c1a12db0a5b1a202e73ab31
dandanT-T <kaiz@rose-hulman.edu>
2020-09-13 13:39:20 -0400

    hello button

--- Follow Alongs/Hello Button/public/scripts/main.js
+++ Follow Alongs/Hello Button/public/scripts/main.js
@@ -1,5 +1,24 @@
+var counter = 0;
+
 function main(){
     console.log("ready");
+    document.querySelector("#decButton").onclick = (event) => {
+        counter--;
+        updateView();
+    }
+    document.querySelector("#resetButton").onclick = (event) => {
+        counter=0;
+        updateView();
+    }
+    document.querySelector("#incButton").onclick = (event) => {
+        counter++;
+        updateView();
+    }
+}
+
+function updateView(){
+    // document.querySelector("#counter").innerHTML = "Counter = "+counter;
+    document.querySelector("#counter").innerHTML = `Count = ${counter}`;
 }
 
 main();
