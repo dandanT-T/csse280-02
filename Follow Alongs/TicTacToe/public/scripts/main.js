199f437ada8092001cc119aafa6a4552f4950d77
dandanT-T <kaiz@rose-hulman.edu>
2020-09-22 09:50:33 -0400

    instance variables

--- Follow Alongs/TicTacToe/public/scripts/main.js
+++ Follow Alongs/TicTacToe/public/scripts/main.js
@@ -1,25 +1,15 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
-
-/** namespace. */
 var rhit = rhit || {};
 
 /** globals */
 rhit.variableName = "";
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
 
-rhit.ClassName = class {
+rhit.PageController = class {
+	
+
 	constructor() {
-
+		// Enable the on chilck listeners
+		this.game = new rhit.Game();
 	}
 
 	methodName() {
@@ -27,10 +17,49 @@ rhit.ClassName = class {
 	}
 }
 
+rhit.Game = class {
+	static Mark = {
+		X: "X",
+		O: "O",
+		NONE: " "
+	}
+
+	static State = {
+		X_TURN: "X's Turn",
+		O_TURN: "O's Turn",
+		X_WIN: "X Wins!",
+		O_WIN: "O Wins!",
+		TIE: "TIE Game"
+	}
+	constructor() {
+		// TODO: Make instance variables
+		this.state = rhit.Game.State.X_TURN;
+		this.board = [];
+		for (let k = 0; k < 9; k++) {
+			this.board.push(rhit.Game.Mark.NONE);
+		}
+		console.log(this.board);
+		console.log('this.board = ', this.board);
+		console.log('this.state = ', this.state);
+	}
+	pressButtonAtIndex(buttonIndex) {
+
+	}
+
+	getMarkAtIndex(buttonIndex) {
+		return "X";
+	}
+
+	getState() {
+		return "X's Turn";
+	}
+}
+
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+	new rhit.PageController();
 };
 
 rhit.main();
