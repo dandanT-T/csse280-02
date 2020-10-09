d86239db36ef3eae7304d5dd82defcd3ecab9c94
dandanT-T <kaiz@rose-hulman.edu>
2020-10-09 18:36:43 -0400

    js scaffolding

--- Follow Alongs/MovieQuotesNoAuth/public/scripts/main.js
+++ Follow Alongs/MovieQuotesNoAuth/public/scripts/main.js
@@ -1,37 +1,63 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
 
-/** namespace. */
 var rhit = rhit || {};
 
-/** globals */
-rhit.variableName = "";
+rhit.FB_COLLECTION_MOVIEQUOTE = "MovieQuotes";
+rhit.FB_KEY_QUOTE = "quote";
+rhit.FB_KEY_MOVIE = "movie";
+rhit.FB_KEY_LAST_TOUCHED = "lastTouched";
+rhit.fbMovieQuoteManager = null;
 
 /** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
+rhit.ListPageController = class {
+	constructor(){
+		console.log("created ListPageController");
+	}
+	updateList(){
+
+	}
 };
 
-rhit.ClassName = class {
-	constructor() {
-
+rhit.NovieQuote = class{
+	constructor(id, quote,movie){
+		this.id = id;
+		this.quote = quote;
+		this.movie = movie;
+		
 	}
-
-	methodName() {
-
-	}
+	
 }
+
+rhit.FbMovieQuotesManager = class {
+	constructor() {
+		console.log("created FbMovieQuotesManager");
+	  this._documentSnapshots = [];
+	  this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_MOVIEQUOTE);
+	}
+	add(quote, movie) {    }
+	beginListening(changeListener) {    }
+	stopListening() {    }
+	// update(id, quote, movie) {    }
+	// delete(id) { }
+	get length() {    }
+	getMovieQuoteAtIndex(index) {    }
+   }
+   
+
 
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+	if(document.querySelector("#listPage")){
+		console.log("listpage");
+		rhit.fbMovieQuoteManager = new rhit.FbMovieQuotesManager();
+		new rhit.ListPageController();
+		// console.log(object);
+	}
 
+	if(document.querySelector("#detailPage")){
+		console.log("detail page");
+	}
 	// Temp code for read and add
 	// const ref = firebase.firestore().collection("MovieQuotes");
 	// ref.onSnapshot((querySnapshot) => {
